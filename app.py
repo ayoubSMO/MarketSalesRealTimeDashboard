546bda6a2f17061473865092890c8199ac1eeba9
Ayoub Boureqba <84507906+ayoubSMO@users.noreply.github.com>
2023-04-06 12:10:02 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print 'hello world !! '
+import streamlit
+import pandas as pd
+import requests
+import snowflake.connector
+from urllib.error import URLError
+
+streamlit.title('🍞 My Streamlit App')
+streamlit.header('🥑 November 17th')
+streamlit.text('🐔 Michigan')
+streamlit.text('🥗 Illinois')
