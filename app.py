9536665f47eb243594ac27ef587605971403bf14
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 09:53:38 +0000

    Df

--- app.py
+++ app.py
@@ -3,13 +3,19 @@ import pandas as pd
 import requests
 import snowflake.connector
 from urllib.error import URLError
+from streamlit_autorefresh import st_autorefresh
 
 def get_all_record_from_snowFlake():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from SUPER_MARKET_SALES.RECORDS.SALES_RECORDS")
     return my_cur.fetchall()
   
-  
+
+
+
+
+
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_data_rows= get_all_record_from_snowFlake()
 df = streamlit.dataframe(my_data_rows)
+st_autorefresh(interval=2000, limit=100, key="dataframe")
