25f335ef515c1311ec87fffb266bd4c123400706
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 11:57:17 +0000

    auto commit

--- app.py
+++ app.py
@@ -19,8 +19,26 @@ my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_data_rows= get_all_record_from_snowFlake()
 data = pd.DataFrame(my_data_rows) 
 data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
+json_list = json.loads(data.to_json(orient='records'))
 
+st.sidebar.header("Please Filter Here:")
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=data["City"].unique(),
+    default=data["City"].unique()
+)
 
+customer_type = st.sidebar.multiselect(
+    "Select the Customer Type:",
+    options=data["Customer_type"].unique(),
+    default=data["Customer_type"].unique(),
+)
+
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=data["Gender"].unique(),
+    default=data["Gender"].unique()
+)
 
 df_selection = data.query(
     "City == @city & Customer_type == @customer_type & Gender == @gender"
@@ -46,30 +64,8 @@ with middle_column:
 with right_column:
     st.subheader("Average Sales Per Transaction:")
     st.subheader(f"US $ {average_sale_by_transaction}")
-
-
-
 df = st.dataframe(data)
-json_list = json.loads(data.to_json(orient='records'))
 
-st.sidebar.header("Please Filter Here:")
-city = st.sidebar.multiselect(
-    "Select the City:",
-    options=data["City"].unique(),
-    default=data["City"].unique()
-)
-
-customer_type = st.sidebar.multiselect(
-    "Select the Customer Type:",
-    options=data["Customer_type"].unique(),
-    default=data["Customer_type"].unique(),
-)
-
-gender = st.sidebar.multiselect(
-    "Select the Gender:",
-    options=data["Gender"].unique(),
-    default=data["Gender"].unique()
-)
 
 st_autorefresh(interval=2000, limit=100, key="dataframe")
 
