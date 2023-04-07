7afa9621fc2481fcfc28233e2e19c4cc4476dfcb
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 10:09:24 +0000

    Df_reff

--- app.py
+++ app.py
@@ -12,9 +12,8 @@ def get_all_record_from_snowFlake():
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_data_rows= get_all_record_from_snowFlake()
-df = streamlit.dataframe(my_data_rows)
+df = streamlit.dataframe(my_data_rows ,columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"])
 
-df.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
 
 st_autorefresh(interval=2000, limit=100, key="dataframe")
 
