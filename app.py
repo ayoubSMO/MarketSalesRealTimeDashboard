a4630aa3626a402a675fdec58702d691e8c71b87
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 10:41:51 +0000

    Add Charts

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 from streamlit_autorefresh import st_autorefresh
+from streamlit_elements import elements, mui, html
 
 # Function that fetch all data from snowflake table 
 def get_all_record_from_snowFlake():
@@ -17,7 +18,30 @@ data = pd.DataFrame(my_data_rows)
 data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
 df = streamlit.dataframe(data)
 
+# First, import the elements you need
 
+
+# Create a frame where Elements widgets will be displayed.
+#
+# Elements widgets will not render outside of this frame.
+# Native Streamlit widgets will not render inside this frame.
+#
+# elements() takes a key as parameter.
+# This key can't be reused by another frame or Streamlit widget.
+
+with elements("new_element"):
+
+    # Let's create a Typography element with "Hello world" as children.
+    # The first step is to check Typography's documentation on MUI:
+    # https://mui.com/components/typography/
+    #
+    # Here is how you would write it in React JSX:
+    #
+    # <Typography>
+    #   Hello world
+    # </Typography>
+
+    mui.Typography("Hello world")
 
 st_autorefresh(interval=2000, limit=100, key="dataframe")
 
