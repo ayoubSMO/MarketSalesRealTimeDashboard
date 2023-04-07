40bdd79c046c70f752e44265747a48a6889a09b6
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 10:24:32 +0000

    Df_reff

--- app.py
+++ app.py
@@ -4,7 +4,10 @@ import requests
 import snowflake.connector
 from urllib.error import URLError
 from streamlit_autorefresh import st_autorefresh
+from streamlit_apexjs import st_apexcharts
 
+
+# Function that fetch all data from snowflake table 
 def get_all_record_from_snowFlake():
   with my_cnx.cursor() as my_cur:
     my_cur.execute("select * from SUPER_MARKET_SALES.RECORDS.SALES_RECORDS")
@@ -16,6 +19,26 @@ data = pd.DataFrame(my_data_rows)
 data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
 df = streamlit.dataframe(data)
 
+
+
+# Chart essay !! 
+options = {
+    "chart": {
+        "toolbar": {
+            "show": False
+        }
+    },
+
+    "labels": data["City"]
+    ,
+    "legend": {
+        "show": True,
+        "position": "bottom",
+    }
+}
+
+series = [44, 55, 41, 17, 15]
+st_apexcharts(options, series, 'donut', '600', 'title')
 
 st_autorefresh(interval=2000, limit=100, key="dataframe")
 
