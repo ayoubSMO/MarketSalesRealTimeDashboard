c8de55b13d5e5da47450f228bf47a92f23488d03
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 11:26:49 +0000

    Add Charts

--- app.py
+++ app.py
@@ -22,67 +22,22 @@ data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product l
 df = st.dataframe(data)
 json_list = json.loads(data.to_json(orient='records'))
 
-"""
-with elements("nivo_charts"):
+city = st.sidebar.multiselect(
+    "Select the City:",
+    options=df["City"].unique(),
+    default=df["City"].unique()
+)
 
-    # Streamlit Elements includes 45 dataviz components powered by Nivo.
+customer_type = st.sidebar.multiselect(
+    "Select the Customer Type:",
+    options=df["Customer_type"].unique(),
+    default=df["Customer_type"].unique(),
+)
 
-
-    dataa = [
-        { "taste": "fruity", "chardonay": 93, "carmenere": 61, "syrah": 114 },
-        { "taste": "bitter", "chardonay": 91, "carmenere": 37, "syrah": 72 },
-        { "taste": "heavy", "chardonay": 56, "carmenere": 95, "syrah": 99 },
-        { "taste": "strong", "chardonay": 64, "carmenere": 90, "syrah": 30 },
-        { "taste": "sunny", "chardonay": 119, "carmenere": 94, "syrah": 103 },
-    ]
-
-    with mui.Box(sx={"height": 500}):
-        nivo.Radar(
-            data=json_list,
-            keys=["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"],
-            indexBy=["Male" , "Female"],
-            valueFormat=">-.2f",
-            margin={ "top": 70, "right": 80, "bottom": 40, "left": 80 },
-            borderColor={ "from": "color" },
-            gridLabelOffset=36,
-            dotSize=10,
-            dotColor={ "theme": "background" },
-            dotBorderWidth=2,
-            motionConfig="wobbly",
-            legends=[
-                {
-                    "anchor": "top-left",
-                    "direction": "column",
-                    "translateX": -50,
-                    "translateY": -40,
-                    "itemWidth": 80,
-                    "itemHeight": 20,
-                    "itemTextColor": "#999",
-                    "symbolSize": 12,
-                    "symbolShape": "circle",
-                    "effects": [
-                        {
-                            "on": "hover",
-                            "style": {
-                                "itemTextColor": "#000"
-                            }
-                        }
-                    ]
-                }
-            ],
-            theme={
-                "background": "#FFFFFF",
-                "textColor": "#31333F",
-                "tooltip": {
-                    "container": {
-                        "background": "#FFFFFF",
-                        "color": "#31333F",
-                    }
-                }
-            }
-        )
-"""
-
-st.bar_chart(json_list)
+gender = st.sidebar.multiselect(
+    "Select the Gender:",
+    options=df["Gender"].unique(),
+    default=df["Gender"].unique()
+)
 st_autorefresh(interval=2000, limit=100, key="dataframe")
 
