36406966682d50d9a38ba5689af7cd2e47a90166
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 11:56:29 +0000

    auto commit

--- app.py
+++ app.py
@@ -9,8 +9,25 @@ from streamlit_elements import elements, mui, html, sync,editor, lazy,nivo
 
 
 
+# Function that fetch all data from snowflake table 
+def get_all_record_from_snowFlake():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("select * from SUPER_MARKET_SALES.RECORDS.SALES_RECORDS")
+    return my_cur.fetchall()
+
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
+my_data_rows= get_all_record_from_snowFlake()
+data = pd.DataFrame(my_data_rows) 
+data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
+
+
+
+df_selection = data.query(
+    "City == @city & Customer_type == @customer_type & Gender == @gender"
+)
+
 # ---- MAINPAGE ----
-st.title(":bar_chart: Market Sales Dashboard")
+st.title(":bar_chart: Sales Dashboard")
 st.markdown("##")
 
 # TOP KPI's
@@ -30,16 +47,8 @@ with right_column:
     st.subheader("Average Sales Per Transaction:")
     st.subheader(f"US $ {average_sale_by_transaction}")
 
-# Function that fetch all data from snowflake table 
-def get_all_record_from_snowFlake():
-  with my_cnx.cursor() as my_cur:
-    my_cur.execute("select * from SUPER_MARKET_SALES.RECORDS.SALES_RECORDS")
-    return my_cur.fetchall()
 
-my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
-my_data_rows= get_all_record_from_snowFlake()
-data = pd.DataFrame(my_data_rows) 
-data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
+
 df = st.dataframe(data)
 json_list = json.loads(data.to_json(orient='records'))
 
@@ -61,11 +70,6 @@ gender = st.sidebar.multiselect(
     options=data["Gender"].unique(),
     default=data["Gender"].unique()
 )
-
-df_selection = data.query(
-    "City == @city & Customer_type == @customer_type & Gender == @gender"
-)
-
 
 st_autorefresh(interval=2000, limit=100, key="dataframe")
 
