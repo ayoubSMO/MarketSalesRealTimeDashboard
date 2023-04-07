20f81413b7390b310174390e054a8e07a95d3688
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-07 13:57:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -78,9 +78,11 @@ df = st.dataframe(data)
 
 st.markdown("""---""")
 
+sum = df_selection["Total"].astype(float)
+
 # SALES BY PRODUCT LINE [BAR CHART]
 sales_by_product_line = (
-    df_selection.astype(str).groupby(by=["Product line"]).sum()[["Total"]].sort_values(by="Total")
+    df_selection.astype(str).groupby(by=["Product line"]).sum()[sum].sort_values(by="Total")
 )
 fig_product_sales = px.bar(
     sales_by_product_line,
