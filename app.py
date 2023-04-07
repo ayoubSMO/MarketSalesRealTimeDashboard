32e4c9e5de7c9f077d442e26666d5cd1774ad543
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 11:52:34 +0000

    auto commit

--- app.py
+++ app.py
@@ -45,16 +45,26 @@ df_selection = data.query(
     "City == @city & Customer_type == @customer_type & Gender == @gender"
 )
 
-# Check data type of 'Total' column
-tt = df_selection['Total'].dtype
-tt
-# Check for missing or invalid values in 'Total' column
-gg = df_selection['Total'].isnull().sum()
-gg
+# ---- MAINPAGE ----
+st.title(":bar_chart: Sales Dashboard")
+st.markdown("##")
 
-# Check for syntax errors or typos in code
-hh = df_selection.loc[:, 'Total'].astype(float).sum()
-hh
+# TOP KPI's
+total_sales = int(df_selection["Total"].astype(float).sum())
+average_rating = round(df_selection["Rating"].astype(float).mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection["Total"].astype(float).mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction}")
 
 
 st_autorefresh(interval=2000, limit=100, key="dataframe")
