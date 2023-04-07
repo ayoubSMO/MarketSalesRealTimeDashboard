22539cabed3841a1a78cac053f64d4ce63c3efc2
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 10:47:22 +0000

    Add Charts

--- app.py
+++ app.py
@@ -12,11 +12,11 @@ def get_all_record_from_snowFlake():
     my_cur.execute("select * from SUPER_MARKET_SALES.RECORDS.SALES_RECORDS")
     return my_cur.fetchall()
 
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_data_rows= get_all_record_from_snowFlake()
 data = pd.DataFrame(my_data_rows) 
 data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
-df = streamlit.dataframe(data)
+df = st.dataframe(data)
 
 
 with elements("callbacks_sync"):
