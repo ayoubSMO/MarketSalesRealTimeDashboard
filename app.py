e57db9ee768ac154cc9b3b170f2147ca0aca737a
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 11:08:51 +0000

    Add Charts

--- app.py
+++ app.py
@@ -20,7 +20,6 @@ my_data_rows= get_all_record_from_snowFlake()
 data = pd.DataFrame(my_data_rows) 
 data.columns = ["Invoice ID","Branch","City","Customer_type","Gender","Product line","Unit price","Quantity","Tax 5%","Total","Date","Time","Payment","cogs","gross margin percentage","gross income","Rating"]
 df = st.dataframe(data)
-dict_list = df.to_dict('records')
 json_list = json.loads(df.to_json(orient='records'))
 
 with elements("nivo_charts"):
