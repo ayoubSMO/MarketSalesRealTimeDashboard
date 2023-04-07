4eab024f210cc19b59141e68e8b9da9728b1a43e
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2023-04-07 11:31:34 +0000

    Add Charts

--- app.py
+++ app.py
@@ -23,7 +23,7 @@ df = st.dataframe(data)
 data["City"]
 json_list = json.loads(data.to_json(orient='records'))
 
-
+st.sidebar.header("Please Filter Here:")
 city = st.sidebar.multiselect(
     "Select the City:",
     options=data["City"].unique(),
@@ -41,6 +41,38 @@ gender = st.sidebar.multiselect(
     options=data["Gender"].unique(),
     default=data["Gender"].unique()
 )
+
+df_selection = data.query(
+    "City == @city & Customer_type ==@customer_type & Gender == @gender"
+)
+
+# ---- MAINPAGE ----
+st.title(":bar_chart: Sales Dashboard")
+st.markdown("##")
+
+# TOP KPI's
+total_sales = int(df_selection["Total"].sum())
+average_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+# TOP KPI's
+total_sales = int(df_selection["Total"].sum())
+average_rating = round(df_selection["Rating"].mean(), 1)
+star_rating = ":star:" * int(round(average_rating, 0))
+average_sale_by_transaction = round(df_selection["Total"].mean(), 2)
+
+left_column, middle_column, right_column = st.columns(3)
+with left_column:
+    st.subheader("Total Sales:")
+    st.subheader(f"US $ {total_sales:,}")
+with middle_column:
+    st.subheader("Average Rating:")
+    st.subheader(f"{average_rating} {star_rating}")
+with right_column:
+    st.subheader("Average Sales Per Transaction:")
+    st.subheader(f"US $ {average_sale_by_transaction}")
+
 
 st_autorefresh(interval=2000, limit=100, key="dataframe")
 
