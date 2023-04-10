87e93d29fca979065c2aaa30ea84a1c70d032e97
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-10 09:16:45 +0000

    Update app.py

--- app.py
+++ app.py
@@ -120,10 +120,13 @@ fig_product_sales.update_layout(
 
 # SALES BY HOUR [BAR CHART]
 sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
-fig_hourly_sales = px.line(
+fig_hourly_sales = px.bar(
     sales_by_hour,
     x=sales_by_hour.index,
-    y="Total"
+    y="Total",
+    title="<b>Sales by hour</b>",
+    color_discrete_sequence=["#FF4500"] * len(sales_by_hour),
+    template="plotly_white",
 )
 fig_hourly_sales.update_layout(
     xaxis=dict(tickmode="linear"),
@@ -136,7 +139,21 @@ left_column, right_column = st.columns(2)
 left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 right_column.plotly_chart(fig_product_sales, use_container_width=True)
 
+# SALES BY HOUR [BAR line]
+sales_by_hour = df_selection.groupby(by=["hour"]).sum()[["Total"]]
+fig_hourly_sales = px.bar(
+    sales_by_hour,
+    x=sales_by_hour.index,
+    y="Total",
+    color_discrete_sequence=["#FF4500"] * len(sales_by_hour)
+)
+fig_hourly_sales.update_layout(
+    xaxis=dict(tickmode="linear"),
+    plot_bgcolor="rgba(0,0,0,0)",
+    yaxis=(dict(showgrid=False)),
+)
 
+left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 
 df = st.dataframe(data)
 
