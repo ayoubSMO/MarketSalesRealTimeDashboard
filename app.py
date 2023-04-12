e6ce372a50116ec10eae31dbfa4f27437fc17614
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-12 13:31:40 +0000

    Update app.py

--- app.py
+++ app.py
@@ -168,7 +168,7 @@ right_column.plotly_chart(fig, use_container_width=True)
 
 
 with left_column:
-    st.subheader("Filtred Data:"))
+    st.subheader("Filtred Data:")
 df = st.dataframe(data)
 
 pd.set_option('display.max_rows', None)  # Display all rows
