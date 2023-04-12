f8805e80b4d9efcf44ef689ea200a1d2ef3c0939
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-12 14:55:32 +0000

    Update app.py

--- app.py
+++ app.py
@@ -91,8 +91,7 @@ st.markdown("##")
 
 total_sales = int(df_selection["Total"].astype(float).sum())
 average_rating = round(df_selection["Rating"].astype(float).mean(), 1)
-
-star_rating = f'<span style="color: #f47c13; font-size: 1.5em">&#9733;</span>'
+f'<span style="color: #f47c13">{"❤️" * int(round(average_rating, 0))}</span>'
 
 average_sale_by_transaction = round(df_selection["Total"].astype(float).mean(), 2)
 
