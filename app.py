91a9263626a8c7e72ff5613db71a30b2556cd3c3
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-12 12:28:07 +0000

    Update app.py

--- app.py
+++ app.py
@@ -156,6 +156,11 @@ fig_hourly_sales.update_layout(
 )
 left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 
+dff = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
+dff.loc[dff['pop'] < 2.e6, 'country'] = 'Other countries'
+fig = px.pie(dff, values='pop', names='country', title='Population of European continent')
+fig.show()
+
 df = st.dataframe(data)
 
 
