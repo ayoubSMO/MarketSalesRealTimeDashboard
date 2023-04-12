496150f368abe453236aef217e1a6cd47ddfd6a8
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-12 12:20:12 +0000

    Update app.py
    Increase size of line plot

--- app.py
+++ app.py
@@ -151,6 +151,7 @@ fig_hourly_sales.update_layout(
     xaxis=dict(tickmode="linear"),
     plot_bgcolor="rgba(0,0,0,0)",
     yaxis=(dict(showgrid=False)),
+    width=900
 )
 left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 
