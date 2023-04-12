3ffa58b1fbddf9516ae17eedb0b04eb8b7282215
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-12 12:30:30 +0000

    Update app.py

--- app.py
+++ app.py
@@ -154,12 +154,12 @@ fig_hourly_sales.update_layout(
     yaxis=(dict(showgrid=False)),
    
 )
-right_column.plotly_chart(fig_hourly_sales, use_container_width=True)
+left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 
 dff = px.data.gapminder().query("year == 2007").query("continent == 'Europe'")
 dff.loc[dff['pop'] < 2.e6, 'country'] = 'Other countries'
 fig = px.pie(dff, values='pop', names='country', title='Population of European continent')
-left_column.plotly_chart(fig, use_container_width=True)
+right_column.plotly_chart(fig, use_container_width=True)
 
 df = st.dataframe(data)
 
