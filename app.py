2eeb717170d87ae1984d9e256f88ca381b49c721
KHALID AIT ZI <105863366+khalidaitzi@users.noreply.github.com>
2023-04-12 13:59:52 +0000

    Update app.py

--- app.py
+++ app.py
@@ -88,19 +88,9 @@ st.title(":bar_chart: Market Sales Dashboard")
 st.markdown("##")
 
 # TOP KPI's
-st.markdown(
-    """
-<style>
-.star_rating span svg path {
-        fill: #f47c13 !important;
-    }
-</style>
-""",
-    unsafe_allow_html=True,
-)
 total_sales = int(df_selection["Total"].astype(float).sum())
 average_rating = round(df_selection["Rating"].astype(float).mean(), 1)
-star_rating = ":star:" * int(round(average_rating, 0))
+star_rating = f'<span style="color: #f47c13">{":star:" * int(round(average_rating, 0))}</span>'
 average_sale_by_transaction = round(df_selection["Total"].astype(float).mean(), 2)
 
 left_column, middle_column, right_column = st.columns(3)
